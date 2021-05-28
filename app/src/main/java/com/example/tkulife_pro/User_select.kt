33b9ddfdfa63bd4dba3096fa83971bc713326aa4
hsuan97330 <jenna8807091010201@gmail.com>
2021-05-28 16:43:33 +0800

    imageBUTTON2

---
+++ app/src/main/java/com/example/tkulife_pro/User_select.kt
@@ -0,0 +1,19 @@
+package com.example.tkulife_pro
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+
+class User_select : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_select)
+//        imageButton2.setOnClickListener {
+//            val no_access= AlertDialog.Builder(this)
+//            no_access.setMessage("您沒有管理員權限")
+//            no_access.setTitle("錯誤")
+//            no_access.setPositiveButton("我知道了",null)
+//            no_access.show()
+//        }
+    }
+}
