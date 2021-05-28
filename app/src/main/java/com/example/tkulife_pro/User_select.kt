158646b4ef4fc975d73c66f3afa978607b2a810d
hsuan97330 <jenna8807091010201@gmail.com>
2021-05-28 17:09:29 +0800

    ImageButton2提示框

--- app/src/main/java/com/example/tkulife_pro/User_select.kt
+++ app/src/main/java/com/example/tkulife_pro/User_select.kt
@@ -3,16 +3,18 @@ package com.example.tkulife_pro
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AlertDialog
-import com.example.tkulife_pro.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_user_select.*
 
-
 class User_select : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_user_select)
-
+        imageButton2.setOnClickListener {
+            val no_access= AlertDialog.Builder(this)
+            no_access.setMessage("您沒有管理員權限")
+            no_access.setTitle("錯誤")
+            no_access.setPositiveButton("我知道了",null)
+            no_access.show()
+        }
     }
 }
