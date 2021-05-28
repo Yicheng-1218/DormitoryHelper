4b1e5e3ff2fa3cfe146ea733d3947978293f6fed
Yicheng-1218 <yicheng.881218@gmail.com>
2021-05-28 17:03:58 +0800

    頁面切換

--- app/src/main/java/com/example/tkulife_pro/MainActivity.kt
+++ app/src/main/java/com/example/tkulife_pro/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.tkulife_pro
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.tkulife_pro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.imageButton.setOnClickListener {
+            Intent(this,User_select::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
