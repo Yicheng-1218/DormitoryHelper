50fdd9699c0be40387c60d3b0c044815ce30837e
Yicheng-1218 <yicheng.881218@gmail.com>
2021-05-31 14:29:16 +0800

    新增清除帳密按鈕

--- app/src/main/java/com/example/tkulife_pro/User_select.kt
+++ app/src/main/java/com/example/tkulife_pro/User_select.kt
@@ -29,12 +29,22 @@ class User_select : AppCompatActivity() {
                 startActivity(this)
             }
         }
+        //TODO 管理員視窗
         binding.imageButton2.setOnClickListener {
             val no_access= AlertDialog.Builder(this)
             no_access.setMessage("您沒有管理員權限")
             no_access.setTitle("錯誤")
             no_access.setPositiveButton("我知道了",null)
             no_access.show()
+        }
+        //TODO 確認視窗
+        binding.button6.setOnClickListener {
+            val confirm = AlertDialog.Builder(this)
+            confirm.setMessage("是否清除帳密")
+            confirm.setTitle("確認視窗")
+            confirm.setNegativeButton("是",null)
+            confirm.setPositiveButton("否",null)
+            confirm.show()
         }
     }
 
