367171de7916a520a548849e5238099d648293c1
star-88 <emma774922@gmail.com>
2021-07-17 23:17:07 +0800

    新增狀況說明頁

---
+++ app/src/main/java/com/example/tkulife_pro/admin/fixreport/FixTab/DescribePage.kt
@@ -0,0 +1,11 @@
+package com.example.tkulife_pro.admin.fixreport.FixTab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DescribePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_describe_page)
+    }
+}
