eee6e0053a952799a67c58c3ce636cca5f83895e
star-88 <emma774922@gmail.com>
2021-07-18 13:55:46 +0800

    新增狀況說明頁AlertDialog

--- app/src/main/java/com/example/tkulife_pro/admin/fixreport/FixTab/DescribePage.kt
+++ app/src/main/java/com/example/tkulife_pro/admin/fixreport/FixTab/DescribePage.kt
@@ -2,19 +2,32 @@ package com.example.tkulife_pro.admin.fixreport.FixTab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import com.example.tkulife_pro.R
 import com.example.tkulife_pro.databinding.ActivityDescribePageBinding
 
 class DescribePage : AppCompatActivity() {
-    private lateinit var binding:ActivityDescribePageBinding
+    private lateinit var binding: ActivityDescribePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivityDescribePageBinding.inflate(layoutInflater)
+        binding = ActivityDescribePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         initView()
+
+
     }
-    private fun initView(){
+
+    private fun initView() {
         val rep = intent.getStringExtra("rep")
         binding.editText.text = rep
+
+        binding.button15.setOnClickListener() {
+            val confirm = AlertDialog.Builder(this)
+            confirm.setMessage("確認要將狀態改為故障中嗎?")
+            confirm.setTitle("確認視窗")
+            confirm.setNegativeButton("確定", null)
+            confirm.setPositiveButton("取消", null)
+            confirm.show()
+        }
     }
 }
