66048196213c3d6dfed7c8730d838463f01e5aae
hsuan97330 <jenna8807091010201@gmail.com>
2021-07-18 22:22:55 +0800

    新的狀態頁

---
+++ app/src/main/java/com/example/tkulife_pro/student/laundry/status/machinestatus/S_Adapter.kt
@@ -0,0 +1,25 @@
+package com.example.tkulife_pro.student.laundry.status.machinestatus
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tkulife_pro.databinding.MachineItemBinding
+import com.example.tkulife_pro.student.laundry.status.machinestatus.S_Adapter
+
+class S_Adapter: RecyclerView.Adapter<S_Adapter.ViewHolder>(){
+
+    class ViewHolder(val view: MachineItemBinding): RecyclerView.ViewHolder(view.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemBinding= MachineItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(itemBinding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
