39cad7e05742cab1245118bfea0b615695b6ec19
Millie8911 <407630861@gms.tku.edu.tw>
2021-07-30 01:03:10 +0800

    package notification顯示寄件人

--- app/src/main/java/com/example/tkulife_pro/FCMService.kt
+++ app/src/main/java/com/example/tkulife_pro/FCMService.kt
@@ -4,6 +4,7 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -17,7 +18,19 @@ class FCMService : FirebaseMessagingService(){
     override fun onCreate() {
         super.onCreate()
         val token = FirebaseMessaging.getInstance().token
-        Log.d("fcm", "refresh token:$token")
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w("fcm", "Fetching FCM registration token failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new FCM registration token
+                val token = task.result
+
+                // Log and toast
+                Log.d("fcm", token.toString())
+
+            })
     }
 
     override fun onNewToken(token: String) {
@@ -25,7 +38,6 @@ class FCMService : FirebaseMessagingService(){
         Log.d("fcm", "refresh token:$token")
     }
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // ...
 
         // TODO(developer): Handle FCM messages here.
         // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
@@ -37,7 +49,7 @@ class FCMService : FirebaseMessagingService(){
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
             Log.d("fcm", "Message Notification Body: ${it.body}")
-            TkuNotification(this,"包裹提醒","包裹提醒").build("包裹提醒","您目前有包裹需領取!${remoteMessage.data}").show(2)
+            TkuNotification(this,"包裹提醒","包裹提醒").build("包裹提醒","您目前有包裹需領取! 寄件人:${remoteMessage.data["寄件人"]}").show(2)
         }
 
         // Also if you intend on generating your own notifications as a result of a received FCM
