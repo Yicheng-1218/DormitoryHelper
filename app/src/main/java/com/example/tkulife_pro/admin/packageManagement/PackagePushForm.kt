e041469aae6283911aee775b26669388cb2b6cd0
Yicheng-1218 <yicheng.881218@gmail.com>
2021-08-04 17:50:57 +0800

    實作管理員包裹提醒發送邏輯

--- app/src/main/java/com/example/tkulife_pro/admin/packageManagement/PackagePushForm.kt
+++ app/src/main/java/com/example/tkulife_pro/admin/packageManagement/PackagePushForm.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.fragment.app.DialogFragment
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import com.example.tkulife_pro.R
+import com.example.tkulife_pro.OkHttpUtil
 import com.example.tkulife_pro.databinding.PackageNotificationFormBinding
+import com.example.tkulife_pro.OkHttpUtil.Companion.mOkHttpUtil
+import okhttp3.Response
+import org.json.JSONObject
+import java.io.IOException
 
-class PackagePushForm: Fragment() {
+class PackagePushForm(val userData:HashMap<*,*>): Fragment() {
 
     private lateinit var binding: PackageNotificationFormBinding
 
@@ -20,7 +24,51 @@ class PackagePushForm: Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding= PackageNotificationFormBinding.inflate(layoutInflater)
-
+        initView()
         return binding.root
     }
+
+
+    private fun initView(){
+//        loading圖示
+        binding.progressBar5.isVisible=false
+//        床號
+        binding.bedNumber.text=userData["room_bed"].toString()
+//        學號
+        binding.stdNumber.text=userData["uid"].toString()
+//        姓名
+        binding.stdName.text=userData["name"].toString()
+//        發送按鈕
+        binding.button19.setOnClickListener {
+            sendPackage()
+        }
+    }
+
+//    發送包裹
+    private fun sendPackage(){
+        binding.progressBar5.isVisible=true
+        binding.button19.isEnabled=false
+        val pid: String =binding.packageId.editableText.toString()
+        if (pid.isNotEmpty()){
+            val body=JSONObject("{'room_bed':'${userData["room_bed"]}','pid':'$pid','token':'${userData["token"]}'}")
+            mOkHttpUtil.post("https://tkudorm.site/package",body,object :OkHttpUtil.ICallback{
+                override fun onResponse(response: Response) {
+                    val res=response.body?.string()
+                    val json=JSONObject(res)
+                    activity?.runOnUiThread {
+                        Toast.makeText(requireContext(),"${json["msg"]}",Toast.LENGTH_SHORT).show()
+                        binding.progressBar5.isVisible=false
+                        binding.button19.isEnabled=true
+                    }
+                }
+
+                override fun onFailure(e: IOException) {
+
+                }
+
+            })
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/tkulife_pro/admin/packageManagement/PushNotification.kt
+++ app/src/main/java/com/example/tkulife_pro/admin/packageManagement/PushNotification.kt
@@ -2,25 +2,105 @@ package com.example.tkulife_pro.admin.packageManagement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import androidx.core.view.isVisible
-import com.example.tkulife_pro.R
-import com.example.tkulife_pro.databinding.ActivityPushNotificationBinding
+import android.util.Log
+import android.widget.Toast
 import com.example.tkulife_pro.databinding.ActivityPushPackagenotificationBinding
+import com.google.firebase.firestore.DocumentReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
 
 class PushNotification : AppCompatActivity() {
     private lateinit var binding: ActivityPushPackagenotificationBinding
+    private lateinit var database:FirebaseFirestore
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityPushPackagenotificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initView()
+    }
 
-//        binding.fragmentContainerView.isVisible=false
-        binding.imageButton18.setOnClickListener{
-//            var dialog=PackagePushForm()
-//            dialog.show(supportFragmentManager,"notificationFrom")
-//            binding.fragmentContainerView.isVisible=true
-            supportFragmentManager.beginTransaction().replace(binding.fragmentContainerView.id,PackagePushForm()).commit()
+
+    private fun initView(){
+//        初始化db
+        database=Firebase.firestore
+
+//        查詢按鈕
+        binding.imageButton18.setOnClickListener {
+//            取得editText.text
+            val roomID=binding.roomBedText.editableText.toString()
+
+//            取得用戶資料
+            getUserDetail(roomID)
         }
+    }
+
+
+    private fun getUserDetail(roomID:String){
+        val data= hashMapOf<String,Any>()
+        try {
+//            定義stdRef欄位
+            var stdRef:DocumentReference? = null
+
+//            取得roomList集合內的roomID文件
+            val roomRef=database.collection("roomList").document(roomID)
+
+//            取得roomID監聽
+            roomRef.get().addOnSuccessListener { doc ->
+
+//                如果資料不為空
+                if (doc!=null && doc.data!=null){
+                    Log.d("user roomRef",doc.data.toString())
+
+//                    初始化stdRef
+                    stdRef=doc.data!!["stdRef"] as DocumentReference
+
+//                    final map 加入roomID
+                    data["room_bed"] = roomID
+                }else{
+//                    查無床號
+                    Log.d("package","room_bed not found")
+                }
+
+            }.continueWith {
+//                接續roomRef執行序
+
+//                取得stdRef監聽
+                stdRef?.get()?.addOnSuccessListener { doc->
+//                    如果資料不為空
+                    if (doc!=null && doc.data!=null){
+//                        temp=學生資料
+                        val temp=doc.data as HashMap<String, Any>
+//                        final map合併temp
+                        data.putAll(temp)
+
+//                        uid=學號
+                        val uid=doc.id
+
+//                        final map 加入學號
+                        data["uid"] = uid
+
+//                        印出測試
+                        Log.d("user stdRef",data.toString())
+                        Log.d("user stdRef",uid)
+                        Log.d("user final map",data.toString())
+                        runOnUiThread {
+                            setFragment(data)
+                        }
+
+                    }
+                }
+            }
+
+        }catch (e:Exception){
+            Log.d("package",e.toString())
+            Toast.makeText(this,"輸入框不可為空",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+//    設定Fragment
+    private fun setFragment(userData:HashMap<*,*>){
+        supportFragmentManager.beginTransaction().replace(binding.fragmentContainerView.id,PackagePushForm(userData)).commit()
     }
 }
