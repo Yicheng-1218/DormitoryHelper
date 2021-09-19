508391909577a94c43a6127b2103eb06f620b533
Yicheng-1218 <yicheng.881218@gmail.com>
2021-09-19 14:12:03 +0800

    狀態追蹤加入toast-3

--- app/src/main/java/com/tkuLife/dorm/student/laundry/status/machineStatus/StatusAdapter.kt
+++ app/src/main/java/com/tkuLife/dorm/student/laundry/status/machineStatus/StatusAdapter.kt
@@ -88,7 +88,7 @@ class StatusAdapter(val context: Context):RecyclerView.Adapter<StatusAdapter.Vie
                         setMessage("完成後提醒我")
                         setNegativeButton("取消",null)
                         setPositiveButton("確定"){ _,_->
-                            Toast.makeText(context,"幫你追蹤瞜<3",Toast.LENGTH_SHORT).show()
+                            Toast.makeText(context,"幫你追蹤瞜(⁎⁍̴̛ᴗ⁍̴̛⁎)",Toast.LENGTH_SHORT).show()
                             Intent(context,NotifyService::class.java).apply {
                                 putExtra("machineID","${machineType}-$num")
                                 context.startForegroundService(this)
